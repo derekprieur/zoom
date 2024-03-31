d0fbe923555f651bc3d3f3bbe1793f62da22a9a1
Derek Prieur <DPrieur123@gmail.com>
2024-03-31 08:34:21 -0400

    update

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,12 @@
+import { StreamVideoProvider } from "@/providers/StreamClientProvider";
 import { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
-  return <main>{children}</main>;
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
 };
 
 export default Layout;
